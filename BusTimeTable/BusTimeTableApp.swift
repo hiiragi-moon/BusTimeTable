e1d44f3c50cec64d4e7e2ee26d544745b6658fa8
hiiragi-moon <shururunrun@gmail.com>
2026-02-22 14:34:40 +0900

    Initial Commit

---
+++ BusTimeTable/BusTimeTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTimeTableApp.swift
+//  BusTimeTable
+//
+//  Created by 新村柊月 on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BusTimeTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BusTimeTable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BusTimeTable
+//
+//  Created by 新村柊月 on 2026/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
